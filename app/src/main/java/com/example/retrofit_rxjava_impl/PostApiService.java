cc49559d6f1430e5727e3c9c3692e6489272d0b7
princeessel <43561228+princeessel@users.noreply.github.com>
2019-09-18 10:21:46 -0500

    Initial commit

--- app/src/main/java/com/example/retrofit_rxjava_impl/PostApiService.java
+++ app/src/main/java/com/example/retrofit_rxjava_impl/PostApiService.java
@@ -7,29 +7,27 @@ import org.reactivestreams.Subscriber;
 
 import java.util.List;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PostApiService {
-private PostApi postApi;
-private List<Post> postList;
+    private PostApi postApi;
+    private List<Post> postList;
     private static String BASE_URL = "https://jsonplaceholder.typicode.com/";
-private static Retrofit retrofit = null;
-
+    private static Retrofit retrofit = null;
 
 
     public static Retrofit getInstance() {
         if (retrofit == null)
-             retrofit = new Retrofit.Builder()
+            retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build();
         return retrofit;
     }
+
     private PostApiService() {
-}
+    }
 }
